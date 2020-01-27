fe34c13eadf579fb5ae0e5cdc2f45839420052de
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-01-27 20:21:05 +0530

    Create ConditionalStatements.js

---
+++ ConditionalStatements.js
@@ -0,0 +1,13 @@
+function getGrade(score) {
+    let grade;
+    var score;
+    if(25 < score && score <= 30){grade="A";}
+    else if(20 < score && score <= 25){grade="B";}
+    else if(15 < score && score <= 20){grade="C";}
+    else if(10 < score && score <= 15){grade="D";}
+    else if(5 < score && score <= 10){grade="E";}
+    else if(0 < score && score <= 5){grade="F";}
+    // Write your code here
+    
+    return grade;
+}
