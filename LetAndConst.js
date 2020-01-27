17bfd18fcdfc0254deecf60545943da2fdec8a4b
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-01-27 20:20:22 +0530

    Create LetAndConst.js

---
+++ LetAndConst.js
@@ -0,0 +1,26 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'
+    const PI = Math.PI;
+    var r = parseFloat(readLine());
+    // Print the area of the circle:
+    var area = PI * r * r;
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r;
+    console.log(perimeter);
+    
+    
+    // Print the area of the circle:
+    
+    // Print the perimeter of the circle:
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
